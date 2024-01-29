c3f02365b137a59a445bce83ef97f333f9cdb436
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2024-01-29 19:59:03 +0100

    handle window creation handle input

---
+++ source/runtime/ppp_device/private/device_input.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct GLFWwindow;
+
+namespace ppp
+{
+    namespace device
+    {
+        namespace input
+        {
+            void process_input(GLFWwindow* window);
+        }
+    }
+}
