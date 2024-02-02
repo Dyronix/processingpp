fe5d3cfc58bdb384f11de179cf0fd5311cc54038
Dyronix <breuck.d@buas.nl>
2024-02-02 09:35:43 +0100

    Constants API

---
+++ source/runtime/ppp_engine/private/constants.cpp
@@ -0,0 +1,34 @@
+#include "constants.h"
+
+#include <glm/gtc/constants.hpp>
+
+namespace ppp
+{
+    namespace constants
+    {
+        float half_pi() 
+        {
+            return glm::half_pi<float>();
+        }
+        
+        float pi()
+        {
+            return glm::pi<float>();
+        }
+        
+        float quarter_pi()
+        {
+            return glm::quarter_pi<float>();
+        }
+        
+        float two_pi()
+        {
+            return glm::two_pi<float>();
+        }
+
+        float tau()
+        {
+            return glm::tau<float>();
+        }
+    }
+}
---
+++ source/runtime/ppp_engine/public/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace ppp
+{
+    namespace constants
+    {
+        float half_pi();
+        float pi();
+        float quarter_pi();
+        float two_pi();
+        float tau();
+    }
+}
