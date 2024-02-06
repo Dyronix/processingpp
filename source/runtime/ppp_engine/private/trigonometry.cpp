dc05b228015b5ab0af1a3490156b6ecb8298b8af
Dyronix <breuck.d@buas.nl>
2024-02-06 15:00:38 +0100

    Trigonometry

--- source/runtime/ppp_engine/private/trigonometry.cpp
+++ source/runtime/ppp_engine/private/trigonometry.cpp
@@ -4,6 +4,19 @@ namespace ppp
 {
     namespace trigonometry
     {
-        // Nothing to implement
+        namespace internal
+        {
+            AngleMode _angle_mode = AngleMode::RADIANS;
+        }
+
+        void angle_mode(AngleMode mode)
+        {
+            internal::_angle_mode = mode;
+        }
+
+        AngleMode angle_mode()
+        {
+            return internal::_angle_mode;
+        }
     }
 }
--- source/runtime/ppp_engine/public/trigonometry.h
+++ source/runtime/ppp_engine/public/trigonometry.h
@@ -1,46 +1,25 @@
 #pragma once
 
+#include "constants.h"
+
+#include <glm/glm.hpp>
+
 #include <cmath>
 
 namespace ppp
 {
     namespace trigonometry
     {
-        template<typename T>
-        T acos(T v)
+        enum class AngleMode
         {
-            return std::acos(v);
-        }
-        template<typename T>
-        T asin(T v)
-        {
-            return std::asin(v);
-        }
-        template<typename T>
-        T atan(T v)
-        {
-            return std::atan(v);
-        }
-        template<typename T>
-        T atan2(T v)
-        {
-            return std::atan2(v);
-        }
-        template<typename T>
-        T cos(T v)
-        {
-            return std::cos(v);
-        }
-        template<typename T>
-        T sin(T v)
-        {
-            return std::sin(v);
-        }
-        template<typename T>
-        T tan(T v)
-        {
-            return std::tan(v);
-        }
+            DEGREES,
+            RADIANS
+        };
+
+        void angle_mode(AngleMode mode);
+
+        AngleMode angle_mode();
+
         template<typename T>
         T degrees(T rad)
         {
@@ -50,6 +29,62 @@ namespace ppp
         T radians(T deg)
         {
             return deg * static_cast<T>(0.01745329251994329576923690768489f);
+        }
+
+        template<typename T>
+        T heading(const glm::vec<2, T, glm::defaultp>& v)
+        {
+            return std::atan2(v.y, v.x);
+        }
+
+        template<typename T>
+        glm::vec<2, T, glm::defaultp> from_angle(T angle)
+        {
+            T v_x = std::cos(angle);
+            T v_y = std::sin(angle);
+
+            return glm::normalize(glm::vec<2, T, glm::defaultp> { v_x, v_y });
+        }
+
+        template<typename T>
+        glm::vec<2, T, glm::defaultp> random_2d()
+        {
+            float angle = ((rand() % 10) / 10.0f) * constants::two_pi();
+            float length = 1.0f;
+
+            T v_x = length * std::cos(angle);
+            T v_y = length * std::sin(angle);
+
+            return glm::vec<2, T, glm::defaultp> { v_x, v_y };
+        }
+
+        template<typename T>
+        glm::vec<2, T, glm::defaultp> set_magnitude(const glm::vec<2, T, glm::defaultp>& v, T mag)
+        {
+            return glm::normalize(v) * mag;
+        }
+
+        template<typename T>
+        T magnitude_squared(const glm::vec<2, T, glm::defaultp>& v)
+        {
+            const T x = v.x;
+            const T y = v.y;
+            return x * x + y * y;
+        }
+
+        template<typename T>
+        glm::vec<2, T, glm::defaultp> limit(const glm::vec<2, T, glm::defaultp>& v, T max)
+        {
+            glm::vec<2, T, glm::defaultp> limit_v = v;
+
+            const T mag_squared = magnitude_squared<T>(limit_v);
+            if (mag_squared > max * max) 
+            {
+                limit_v /= std::sqrt(mag_squared);
+                limit_v *= max;
+            }
+
+            return limit_v;
         }
     }
 }
