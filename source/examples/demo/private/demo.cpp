a75ccb6fe63512c79376b095a67cffc9df0a955f
Dyronix <breuck.d@buas.nl>
2024-02-08 21:45:59 +0100

    Update the demo example

--- source/examples/demo/private/demo.cpp
+++ source/examples/demo/private/demo.cpp
@@ -7,9 +7,12 @@
 #include "structure.h"
 #include "transform.h"
 #include "environment.h"
+#include "image.h"
 
 namespace ppp
 {
+    image::Image _image;
+
     AppParams entry()
     {
         AppParams app_params;
@@ -24,9 +27,11 @@ namespace ppp
     {
         keyboard::set_quit_application_keycode(keyboard::KeyCode::KEY_ESCAPE);
 
-        //rendering::create_canvas((1280 / 2) - 200, (720 / 2) - 200, 400.0f, 400.0f);
+        rendering::create_canvas((1280 / 2) - 200, (720 / 2) - 200, 400.0f, 400.0f);
 
         color::background(15);
+
+        _image = image::load("blank.png");
     }
 
     void draw()
@@ -35,20 +40,28 @@ namespace ppp
         {
             transform::translate(environment::canvas_width() / 2, environment::canvas_height() / 2);
 
-            transform::push();
-            {
-                color::fill(255);
+            //    transform::push();
+            //    {
+            //        color::fill(255);
+            //        transform::scale(2.0f, 2.0f);
+            //        shapes::rect(0, 0, 100, 100);
+            //    }
+            //    transform::pop();
 
-                transform::scale(2.0f, 2.0f);
-                shapes::rect(0, 0, 100, 100);
-            }
-            transform::pop();
+            shapes::rect_mode(shapes::ShapeMode::CORNER);
+            color::stroke_weight(5);
+            color::stroke(0, 255, 0, 255);
+
+            color::fill(255, 0, 0, 255);
+            shapes::ellipse(-50, 0, 75, 75);
+            shapes::rect(50, 0, 75, 75);
+            shapes::triangle(0, 150, 25, 200, 50, 150);
+            //shapes::rect(150, 0, 100, 100);
+            //shapes::line(0, 0, 100, 100);
+            //shapes::point(0, 0);
+
+            image::draw(_image.id, 0, -100, 100, 100);
         }
-
-        color::fill(255, 0, 0, 255);
-        
-        shapes::rect(0, 0, 100, 100);
-
         transform::pop();
     }
 }
