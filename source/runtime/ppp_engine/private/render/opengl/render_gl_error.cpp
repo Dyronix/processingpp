b5ed3723e489adfe3aa8ac15d653e946b998615d
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2024-10-24 11:32:15 +0200

    opengl error handling

---
+++ source/runtime/ppp_engine/private/render/opengl/render_gl_error.cpp
@@ -0,0 +1,55 @@
+#include "render/opengl/render_gl_error.h"
+
+#include "util/log.h"
+
+#include <glad/glad.h>
+
+namespace ppp
+{
+    namespace render
+    {
+        //-------------------------------------------------------------------------
+        std::string gl_get_error_message(u32 error)
+        {
+            switch (error)
+            {
+                case GL_INVALID_OPERATION: return "Invalid Operation";
+                case GL_INVALID_ENUM: return "Invalid Enum";
+                case GL_INVALID_VALUE: return "Invalid Value";
+                case GL_INVALID_INDEX: return "Invalid Index";
+                case GL_INVALID_FRAMEBUFFER_OPERATION: return "Invalid Framebuffer Operation";
+            }
+
+            return "Unknown error";
+        }
+
+        //-------------------------------------------------------------------------
+        u32 gl_check_error()
+        {
+            u32 error = GL_NO_ERROR;
+
+            error = glGetError();  // no GL_CALL here to avoid recursive function call.
+
+            return error;
+        }
+
+        //-------------------------------------------------------------------------
+        bool gl_log_call(const char* function, const char* file, s32 line)
+        {
+            u32 error = gl_check_error();
+            if (error == GL_NO_ERROR)
+            {
+                return true;
+            }
+
+            while (error != GL_NO_ERROR)
+            {
+                log::error("[OpenGL Error] ({0}, {1}): {2} {3}: {4}", error, gl_get_error_message(error), function, file, line);
+
+                error = gl_check_error();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ source/runtime/ppp_engine/private/render/opengl/render_gl_error.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "util/types.h"
+
+#include <string>
+
+#pragma warning(push)
+#pragma warning(disable : 4505)
+
+namespace ppp
+{
+    namespace render
+    {
+        std::string gl_get_error_message(u32 error);
+
+        u32         gl_check_error();
+
+        bool        gl_log_call(const char* function, const char* file, s32 line);
+    }
+}
+
+#ifdef _DEBUG
+    #define GL_CALL(x)                                                      \
+        ppp::render::gl_check_error();                                      \
+        x;                                                                  \
+        if (!ppp::render::gl_log_call(#x, __FILE__, __LINE__))              \
+        {                                                                   \
+            __debugbreak();                                                 \
+        }
+#else
+    #define GL_CALL(x) x
+#endif
+
+#pragma warning(pop)
