0b47632ed2ff345630010cba8ab26334d9ffbf64
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2024-10-24 11:38:32 +0200

    material

--- source/runtime/ppp_engine/private/material.cpp
+++ source/runtime/ppp_engine/private/material.cpp
@@ -1,20 +1,68 @@
 #include "material.h"
+#include "fileio/fileio.h"
+#include "render/render_shaders.h"
+#include "resources/shader_pool.h"
 
 namespace ppp
 {
     namespace material
     {
+        shader_program::shader_program()
+            :id(-1)
+        {}
+
+        shader_program::shader_program(shader_program_id in_id)
+            :id(in_id)
+        {}
+
+        void shader_program::set_uniform(const std::string& uniform_name, bool value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, int value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, float value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::vec2& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::vec3& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::vec4& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::mat2& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::mat3& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+        void shader_program::set_uniform(const std::string& uniform_name, const glm::mat4& value)
+        {
+            render::shaders::push_uniform(id, uniform_name, value);
+        }
+
         void ambient_color(float r, float g, float b)
         {
             
         }
 
-        void texture(image::image_id id)
+        void texture(unsigned int image_id)
         {
 
         }
 
-        void shader(shader_program_id id)
+        void shader(unsigned int shader_program_id)
         {
 
         }
@@ -34,14 +82,45 @@ namespace ppp
 
         }
 
-        shader_program_id create_shader(const std::string& vertex_source, const std::string& fragment_source)
+        shader_program create_shader(const std::string& tag, const std::string& vertex_source, const std::string& fragment_source)
         {
-            return -1;
+            u32 shader_program_id = shader_pool::add_shader_program(tag, vertex_source.c_str(), fragment_source.c_str());
+            return { shader_program_id };
         }
 
-        shader_program_id load_shader(const std::string& vertex_path, const std::string& fragment_path)
+        shader_program create_shader(const std::string& tag, const std::string& vertex_source, const std::string& fragment_source, const std::string& geometry_source)
         {
-            return -1;
+            u32 shader_program_id = shader_pool::add_shader_program(tag, vertex_source.c_str(), fragment_source.c_str(), geometry_source.c_str());
+            return { shader_program_id };
+        }
+
+        shader_program load_shader(const std::string& tag, const std::string& vertex_path, const std::string& fragment_path)
+        {
+            if (shader_pool::has_shader(tag))
+            {
+                return { shader_pool::get_shader_program(tag) };
+            }
+
+            auto vs_buffer = fileio::read_text_file(vertex_path);
+            auto fs_buffer = fileio::read_text_file(fragment_path);
+
+            u32 shader_program_id = shader_pool::add_shader_program(tag, vs_buffer.c_str(), fs_buffer.c_str());
+            return { shader_program_id };
+        }
+
+        shader_program load_shader(const std::string& tag, const std::string& vertex_path, const std::string& fragment_path, const std::string& geometry_path)
+        {
+            if (shader_pool::has_shader(tag))
+            {
+                return { shader_pool::get_shader_program(tag) };
+            }
+
+            auto vs_buffer = fileio::read_text_file(vertex_path);
+            auto gs_buffer = fileio::read_text_file(geometry_path);
+            auto fs_buffer = fileio::read_text_file(fragment_path);
+
+            u32 shader_program_id = shader_pool::add_shader_program(tag, vs_buffer.c_str(), fs_buffer.c_str(), gs_buffer.c_str());
+            return { shader_program_id };
         }
     }
 }
--- source/runtime/ppp_engine/public/material.h
+++ source/runtime/ppp_engine/public/material.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "image.h"
+#include <glm/glm.hpp>
 
 #include <string>
 
@@ -10,15 +10,35 @@ namespace ppp
     {
         using shader_program_id = unsigned int;
 
+        struct shader_program
+        {
+            shader_program_id id;
+
+            shader_program();
+            shader_program(shader_program_id in_id);
+
+            void set_uniform(const std::string& uniform_name, bool value);
+            void set_uniform(const std::string& uniform_name, int value);
+            void set_uniform(const std::string& uniform_name, float value);
+            void set_uniform(const std::string& uniform_name, const glm::vec2& value);
+            void set_uniform(const std::string& uniform_name, const glm::vec3& value);
+            void set_uniform(const std::string& uniform_name, const glm::vec4& value);
+            void set_uniform(const std::string& uniform_name, const glm::mat2& value);
+            void set_uniform(const std::string& uniform_name, const glm::mat3& value);
+            void set_uniform(const std::string& uniform_name, const glm::mat4& value);
+        };
+
         void ambient_color(float r, float g, float b);
-        void texture(image::image_id id);
-        void shader(shader_program_id id);
+        void texture(unsigned int image_id);
+        void shader(unsigned int shader_program_id);
         void reset();
 
         void normal_material();
         void specular_material();
 
-        shader_program_id create_shader(const std::string& vertex_source, const std::string& fragment_source);
-        shader_program_id load_shader(const std::string& vertex_path, const std::string& fragment_path);
+        shader_program create_shader(const std::string& tag, const std::string& vertex_source, const std::string& fragment_source);
+        shader_program create_shader(const std::string& tag, const std::string& vertex_source, const std::string& fragment_source, const std::string& geometry_source);
+        shader_program load_shader(const std::string& tag, const std::string& vertex_path, const std::string& fragment_path);
+        shader_program load_shader(const std::string& tag, const std::string& vertex_path, const std::string& fragment_path, const std::string& geometry_path);
     }
 }
