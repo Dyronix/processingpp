539becde5dc675c13ab171b8f0f2bddb4046eded
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-01-20 23:25:07 +0100

    Material

--- source/runtime/ppp_engine/private/resources/material.cpp
+++ source/runtime/ppp_engine/private/resources/material.cpp
@@ -5,6 +5,7 @@ namespace ppp
     namespace resources
     {
         //-------------------------------------------------------------------------
+        // Material
         material::material(const std::string& shader_tag)
             : m_shader_tag(shader_tag)
             , m_ambient_color(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f))
@@ -15,24 +16,12 @@ namespace ppp
         }
 
         //-------------------------------------------------------------------------
-        const u64 material::id() const
-        {
-            return m_id;
-        }
-
-        //-------------------------------------------------------------------------
-        bool material::has_textures() const
-        {
-            return textures().size() > 0;
-        }
-
-        //-------------------------------------------------------------------------
         void material::ambient_color(const glm::vec4& ambient_color) { m_ambient_color = ambient_color; }
         //-------------------------------------------------------------------------
         void material::diffuse_color(const glm::vec4& diffuse_color) { m_diffuse_color = diffuse_color; }
 
         //-------------------------------------------------------------------------
-        bool material::add_texture(render::texture_id id)
+        s32 material::add_texture(render::texture_id id)
         {
             if (m_texture_registry.can_add(1))
             {
@@ -48,9 +37,21 @@ namespace ppp
         }
 
         //-------------------------------------------------------------------------
-        const glm::vec4& material::ambient_color() const { return m_ambient_color; }
+        bool material::has_textures() const
+        {
+            return textures().size() > 0;
+        }
+
         //-------------------------------------------------------------------------
-        const glm::vec4& material::diffuse_color() const { return m_diffuse_color; }
+        const u64 material::id() const
+        {
+            return m_id;
+        }
+
+        //-------------------------------------------------------------------------
+        const glm::vec4 material::ambient_color() const { return m_ambient_color; }
+        //-------------------------------------------------------------------------
+        const glm::vec4 material::diffuse_color() const { return m_diffuse_color; }
 
         //-------------------------------------------------------------------------
         const sampler_ids& material::samplers() const { return m_texture_registry.samplers(); }
@@ -59,5 +60,54 @@ namespace ppp
 
         //-------------------------------------------------------------------------
         const std::string& material::shader_tag() const { return m_shader_tag; }
+
+
+        //-------------------------------------------------------------------------
+        // Material Instance
+        material_instance::material_instance(material* base_material)
+            : m_base_material(base_material)
+        {}
+
+        //-------------------------------------------------------------------------
+        void material_instance::ambient_color(const glm::vec4& color) { m_ambient_color = color; }
+        //-------------------------------------------------------------------------
+        void material_instance::diffuse_color(const glm::vec4& color) { m_diffuse_color = color; }
+
+        //-------------------------------------------------------------------------
+        s32 material_instance::add_texture(render::texture_id id)
+        {
+            s32 sampler_id = m_base_material->add_texture(id);
+            if (sampler_id >= 0)
+            {
+                m_samplers.push_back(sampler_id);
+                return sampler_id;
+            }
+
+            return sampler_id;
+        }
+        //-------------------------------------------------------------------------
+        void material_instance::reset_textures()
+        {
+            m_base_material->reset_textures();
+        }
+
+        //-------------------------------------------------------------------------
+        bool material_instance::has_textures() const { return m_base_material->has_textures(); }
+
+        //-------------------------------------------------------------------------
+        const u64 material_instance::id() const { return m_base_material->id(); }
+
+        //-------------------------------------------------------------------------
+        const glm::vec4 material_instance::ambient_color() const { return m_ambient_color.value_or(m_base_material->ambient_color()); }
+        //-------------------------------------------------------------------------
+        const glm::vec4 material_instance::diffuse_color() const { return m_diffuse_color.value_or(m_base_material->diffuse_color()); }
+
+        //-------------------------------------------------------------------------
+        const sampler_ids& material_instance::samplers() const { return m_samplers; }
+        //-------------------------------------------------------------------------
+        const texture_ids& material_instance::textures() const { return m_base_material->textures(); }
+
+        //-------------------------------------------------------------------------
+        const std::string& material_instance::shader_tag() const { return m_base_material->shader_tag(); }
     }
 }
--- source/runtime/ppp_engine/private/resources/material.h
+++ source/runtime/ppp_engine/private/resources/material.h
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include <string>
+#include <optional>
 
 namespace ppp
 {
@@ -16,28 +17,54 @@ namespace ppp
         using sampler_ids = std::vector<s32>;
         using texture_ids = std::vector<render::texture_id>;
 
-        class material
+        class imaterial
+        {
+        public:
+            virtual bool has_textures() const = 0;
+
+            virtual void ambient_color(const glm::vec4& ambient_color) = 0;
+            virtual void diffuse_color(const glm::vec4& diffuse_color) = 0;
+
+            virtual s32 add_texture(render::texture_id id) = 0;
+            virtual void reset_textures() = 0;
+
+            virtual const u64 id() const = 0;
+
+            virtual const glm::vec4 ambient_color() const = 0;
+            virtual const glm::vec4 diffuse_color() const = 0;
+
+            virtual const sampler_ids& samplers() const = 0;
+            virtual const texture_ids& textures() const = 0;
+
+            virtual const std::string& shader_tag() const = 0;
+        };
+
+        class material : public imaterial
         {
         public:
             material(const std::string& shader_tag);
 
-            bool has_textures() const;
+        public:
+            void ambient_color(const glm::vec4& ambient_color) override;
+            void diffuse_color(const glm::vec4& diffuse_color) override;
 
-            void ambient_color(const glm::vec4& ambient_color);
-            void diffuse_color(const glm::vec4& diffuse_color);
+        public:
+            s32 add_texture(render::texture_id id) override;
 
-            bool add_texture(render::texture_id id);
-            void reset_textures();
+            void reset_textures() override;
 
-            const u64 id() const;
+        public:
+            bool has_textures() const override;
 
-            const glm::vec4& ambient_color() const;
-            const glm::vec4& diffuse_color() const;
+            const u64 id() const override;
+
+            const glm::vec4 ambient_color() const override;
+            const glm::vec4 diffuse_color() const override;
             
-            const sampler_ids& samplers() const;
-            const texture_ids& textures() const;
+            const sampler_ids& samplers() const override;
+            const texture_ids& textures() const override;
 
-            const std::string& shader_tag() const;
+            const std::string& shader_tag() const override;
 
         private:
             u64 m_id;
@@ -48,6 +75,42 @@ namespace ppp
             glm::vec4 m_diffuse_color;
 
             render::texture_registry m_texture_registry;
+        };
+
+        class material_instance : public imaterial
+        {
+        public:
+            material_instance(material* base_material);
+
+        public:
+            void ambient_color(const glm::vec4& color);
+            void diffuse_color(const glm::vec4& color);
+
+        public:
+            s32 add_texture(render::texture_id id);
+
+            void reset_textures() override;
+
+        public:
+            bool has_textures() const override;
+
+            const u64 id() const override;
+
+            const glm::vec4 ambient_color() const override;
+            const glm::vec4 diffuse_color() const override;
+
+            const sampler_ids& samplers() const override;
+            const texture_ids& textures() const override;
+
+            const std::string& shader_tag() const override;
+
+        private:
+            material* m_base_material;
+
+            std::optional<glm::vec4> m_ambient_color;
+            std::optional<glm::vec4> m_diffuse_color;
+
+            sampler_ids m_samplers;
         };
     }
 }
