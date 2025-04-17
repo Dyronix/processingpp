673b098f807ea45091763717bc61a9ef5a608a8c
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-04-17 21:22:08 +0200

    use glm for vector and matrix math
    (cherry picked from commit e0fb3c9a2eb5c9eed47b82c29c8fafba57569556)

--- source/runtime/ppp_engine/public/matrix.h
+++ source/runtime/ppp_engine/public/matrix.h
@@ -1,82 +1,25 @@
+/**
+ * @file matrix.h
+ * @brief Defines matrix type aliases for 2x2, 3x3, and 4x4 matrices.
+ */
 #pragma once
 
-#include "vector.h"   // Your existing vector types (base_vec2, base_vec3)
+#include <glm/mat4x4.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat2x2.hpp>
 
 namespace ppp
 {
-    // 2x2 Matrix: uses 2 columns of type base_vec2
-    template<typename T = float>
-    struct base_mat2
-    {
-        base_vec2<T> cols[2];
-
-        // Default constructor initializes to identity matrix:
-        // [ 1 0 ]
-        // [ 0 1 ]
-        base_mat2() : cols{ base_vec2<T>(1, 0),
-                       base_vec2<T>(0, 1) } 
-        {}
-
-        // Optionally, allow construction with specified columns
-        base_mat2(const base_vec2<T>& col0, const base_vec2<T>& col1)
-            : cols{ col0, col1 } 
-        {}
-    };
-
-    // 3x3 Matrix: uses 3 columns of type base_vec3
-    template<typename T = float>
-    struct base_mat3
-    {
-        base_vec3<T> cols[3];
-
-        // Default constructor initializes to identity matrix:
-        // [ 1 0 0 ]
-        // [ 0 1 0 ]
-        // [ 0 0 1 ]
-        base_mat3() : cols{ base_vec3<T>(1, 0, 0),
-                       base_vec3<T>(0, 1, 0),
-                       base_vec3<T>(0, 0, 1) } 
-        {}
-
-        // Optionally, allow construction with specified columns
-        base_mat3(const base_vec3<T>& col0, const base_vec3<T>& col1, const base_vec3<T>& col2)
-            : cols{ col0, col1, col2 } 
-        {}
-    };
-
-    // 4x4 Matrix: uses 4 columns of type vec4
-    template<typename T = float>
-    struct base_mat4 
-    {
-        base_vec4<T> cols[4];
-
-        // Default constructor initializes to identity matrix:
-        // [ 1 0 0 0 ]
-        // [ 0 1 0 0 ]
-        // [ 0 0 1 0 ]
-        // [ 0 0 0 1 ]
-        base_mat4() : cols{ base_vec4<T>(1, 0, 0, 0),
-                       base_vec4<T>(0, 1, 0, 0),
-                       base_vec4<T>(0, 0, 1, 0),
-                       base_vec4<T>(0, 0, 0, 1) } 
-        {}
-
-        // Optionally, allow construction with specified columns
-        base_mat4(const base_vec4<T>& col0, const base_vec4<T>& col1, const base_vec4<T>& col2, const base_vec4<T>& col3)
-            : cols{ col0, col1, col2, col3 } 
-        {}
-    };
-
-    using mat2f = base_mat2<float>;
-    using mat2d = base_mat2<double>;
+    using mat2f = glm::mat<2, 2, float, glm::defaultp>;
+    using mat2d = glm::mat<2, 2, double, glm::defaultp>;
     using mat2 = mat2f;
 
-    using mat3f = base_mat3<float>;
-    using mat3d = base_mat3<double>;
+    using mat3f = glm::mat<3, 3, float, glm::defaultp>;
+    using mat3d = glm::mat<3, 3, double, glm::defaultp>;
     using mat3 = mat3f;
 
-    using mat4f = base_mat4<float>;
-    using mat4d = base_mat4<double>;
+    using mat4f = glm::mat<4, 4, float, glm::defaultp>;
+    using mat4d = glm::mat<4, 4, double, glm::defaultp>;
     using mat4 = mat4f;
 
 } // namespace ppp
