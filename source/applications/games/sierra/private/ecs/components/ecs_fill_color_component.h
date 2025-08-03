e9acbe9ccf769ad22092b80ec59d2996472ba0a9
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-08-03 22:13:28 +0200

    change to vec4i and add a highlight color

--- source/applications/games/sierra/private/ecs/components/ecs_fill_color_component.h
+++ source/applications/games/sierra/private/ecs/components/ecs_fill_color_component.h
@@ -2,16 +2,16 @@
 
 #include "model.h"
 
+#include <glm/glm.hpp>
+
 namespace ppp
 {
     namespace ecs
     {
         struct fill_color_component
         {
-            int r;
-            int g; 
-            int b; 
-            int a;
+            glm::ivec4 color;
+            glm::ivec4 highlight;
         };
     }
 }
