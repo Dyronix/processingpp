cb78099e7722372cd4f4ebfeb5a21d6bcd9f04f3
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-08-03 22:15:48 +0200

    Helper includes

--- source/applications/games/sierra/private/ecs/systems/ecs_systems.h
+++ source/applications/games/sierra/private/ecs/systems/ecs_systems.h
@@ -3,3 +3,4 @@
 #include "ecs/systems/ecs_camera_init_system.h"
 #include "ecs/systems/ecs_orbit_camera_system.h"
 #include "ecs/systems/ecs_render_system.h"
+#include "ecs/systems/ecs_picking_system.h"
