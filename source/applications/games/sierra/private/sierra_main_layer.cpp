7be655670097c16b1866f9d17339454d684c84c8
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-08-04 17:46:07 +0200

    remove duplicate or unused code

--- source/applications/games/sierra/private/sierra_main_layer.cpp
+++ source/applications/games/sierra/private/sierra_main_layer.cpp
@@ -13,85 +13,6 @@
 
 namespace ppp
 {
-	struct level_builder_config2
-	{
-		s32 tile_size = 1;
-		s32 tile_spacing = 0;
-		s32 xoffset = 0;
-		s32 zoffset = 0;
-	};
-
-  tile_type tile_type_from_string(std::string_view type)
-  {
-    if (type == "grass") return ppp::tile_type::grass;
-    if (type == "path") return ppp::tile_type::path;
-    if (type == "water") return ppp::tile_type::water;
-    if (type == "begin") return ppp::tile_type::begin;
-    if (type == "end") return ppp::tile_type::end;
-
-    return (tile_type)-1;
-  }
-
-  glm::vec4 color_from_tile_type(tile_type type)
-  {
-    switch (type)
-    {
-    case ppp::tile_type::grass:  return glm::vec4(0, 255, 0, 255);
-    case ppp::tile_type::path:   return glm::vec4(245, 147, 66, 255);
-    case ppp::tile_type::water:  return glm::vec4(66, 194, 245, 255);
-    case ppp::tile_type::begin:  return glm::vec4(224, 66, 245, 255);
-    case ppp::tile_type::end:    return glm::vec4(0, 0, 0, 0);
-    default:    return glm::vec4();
-    }
-  }
-
-  //-------------------------------------------------------------------------
-  flecs::entity create_tile(sierra_layer* layer, s32 x, s32 z, const std::string& type, const level_builder_config2& config)
-  {
-    std::stringstream name;
-    name << type << x << z << "2";
-
-    flecs::entity e = layer->create_entity(name.str().c_str());
-
-    f32 final_x = ((x * config.tile_size) + config.xoffset) + (x * config.tile_spacing);
-    f32 final_y = 0.0f;
-    f32 final_z = ((z * config.tile_size) + config.zoffset) + (z * config.tile_spacing);
-
-    glm::vec3 center = { final_x, final_y, final_z };
-    tile_type tile_type = tile_type_from_string(type);
-    e.set<ecs::transform_component>({ center, {1, 1, 1}, glm::quat(1, 0, 0, 0) });
-    e.set<ecs::shape_component>({ [=]() { box((f32)config.tile_size); } });
-    e.set<ecs::fill_color_component>({ color_from_tile_type(tile_type), {255, 255, 255, 255} });
-
-    f32 half_extent = config.tile_size * 0.5f;
-    e.set<ecs::bounding_box_component>({ glm::vec3(-half_extent), glm::vec3(half_extent) });
-    e.set<ecs::tile_component>({ tile_type });
-    e.add<ecs::pickable_component>();
-
-    return e;
-  }
-
-  //-------------------------------------------------------------------------
-  std::vector<flecs::entity> build_from_json(sierra_layer* layer, const json& level_json, const level_builder_config2& config)
-  {
-    std::vector<flecs::entity> entities;
-
-    for (const auto& row : level_json["matrix"])
-    {
-      for (const auto& tile : row)
-      {
-        s32 x = tile["x"];
-        s32 z = tile["z"];
-        std::string type = tile["type"];
-
-        auto entity = create_tile(layer, x, z, type, config);
-        entities.push_back(entity);
-      }
-    }
-
-    return entities;
-  }
-
     //-------------------------------------------------------------------------
     sierra_main_layer::sierra_main_layer(sierra_engine_context* ctx)
         :sierra_layer(ctx, "main"_sid, 0, false)
@@ -107,19 +28,6 @@ namespace ppp
         create_trigger();
 
         init_systems();
-    }
-
-    void sierra_main_layer::load_level()
-    {
-      json demo_level = load_text_json("local:content/json/demo_level.json");
-
-      level_builder_config2 cfg;
-      cfg.tile_size = demo_level["tile-size"];
-      cfg.tile_spacing = demo_level["tile-spacing"];
-      cfg.xoffset = -cfg.tile_size * 2;
-      cfg.zoffset = -cfg.tile_size * 2;
-
-      //_grid = build_from_json(this, demo_level, cfg);
     }
 
     void sierra_main_layer::create_camera()
--- source/applications/games/sierra/private/sierra_main_layer.h
+++ source/applications/games/sierra/private/sierra_main_layer.h
@@ -2,7 +2,6 @@
 
 #include "sierra_layer.h"
 #include "ecs/components/ecs_components.h"
-#include "grid/a-star-grid.h"
 
 namespace ppp
 {
@@ -18,7 +17,6 @@ namespace ppp
     private:
       // initialization
       void init_systems();
-      void load_level();
 
       // entity Creation
       void create_camera();
@@ -62,8 +60,6 @@ namespace ppp
       f32 _enemy_radius = 50.0f;
       f32 _bullet_radius = 5.0f;
       f32 _current_time;
-
-      grid<flecs::entity> _grid;
 
       std::vector<ecs::transform_component> _begin_transforms;
     };
