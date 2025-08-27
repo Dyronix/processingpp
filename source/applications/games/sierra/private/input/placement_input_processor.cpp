3ca1ca5fcbdb3844e6b95ed84b6b65b5404d52f1
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-08-27 23:23:20 +0200

    input processors

---
+++ source/applications/games/sierra/private/input/placement_input_processor.cpp
@@ -0,0 +1,54 @@
+#include "placement_input_processor.h"
+
+#include "sierra_layer.h"
+#include "sierra_engine_context.h"
+#include "sierra_player_state.h"
+
+#include "util/log.h"
+
+namespace ppp
+{
+    //-------------------------------------------------------------------------
+    placement_input_processor::placement_input_processor(sierra_layer* layer)
+        :_owning_layer(layer)
+    {}
+
+    //-------------------------------------------------------------------------
+    bool placement_input_processor::on_input(const input_event& evt)
+    {
+        switch (evt.type)
+        {
+        case input_event_type::KEY_PRESS: return handle_key_press(evt.keyboard);
+        case input_event_type::MOUSE_PRESS: return handle_mouse_press(evt.mouse_button);
+        }
+
+        return false;
+    }
+
+    //-------------------------------------------------------------------------
+    bool placement_input_processor::handle_mouse_press(const mouse_button_event& evt)
+    {
+        switch (evt.button)
+        {
+        case mouse_code::BUTTON_LEFT:
+            log::info("Going to try and spawn a tower");
+            _owning_layer->context()->player_state.try_spawn_tower();
+            return true;
+        }
+
+        return false;
+    }
+    //-------------------------------------------------------------------------
+    bool placement_input_processor::handle_key_press(const keyboard_event& evt)
+    {
+        switch (evt.code)
+        {
+        case key_code::KEY_SPACE:
+            log::info("Going to change to placement state: {}", !_owning_layer->context()->player_state.is_placing());
+            _owning_layer->context()->player_state.toggle_placement();
+            return true;
+        }
+
+        return false;
+    }
+}
--- source/applications/games/sierra/private/input/ui_input_processor.h
+++ source/applications/games/sierra/private/input/ui_input_processor.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "sierra_input_processor.h"
-
 #include "ecs/components/ecs_components.h"
+
+#include <flecs.h>
 
 namespace ppp
 {
     class sierra_layer;
+
+    struct mouse_button_event;
 
     class ui_input_processor : public isierra_input_processor
     {
@@ -16,6 +19,8 @@ namespace ppp
         bool on_input(const input_event& evt);
 
     private:
+        bool handle_left_mouse_button_click(const mouse_button_event& evt);
+
         using picking_query = flecs::query<
             const class ecs::rect_transform_component,
             const class ecs::bounding_box_2d_component,
