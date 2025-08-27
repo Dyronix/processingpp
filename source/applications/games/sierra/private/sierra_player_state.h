945873826bb9695399bf1f15f49c194288bbdf74
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2025-08-27 23:22:48 +0200

    Player state

--- source/applications/games/sierra/private/sierra_player_state.h
+++ source/applications/games/sierra/private/sierra_player_state.h
@@ -12,10 +12,12 @@ namespace ppp
     public:
         void toggle_placement();
         void try_spawn_tower();
+        void start_new_wave();
 
         void subscribe_start_placement(player_state_delegate callback);
         void subscribe_stop_placement(player_state_delegate callback);
         void subscribe_try_spawn_tower(player_state_delegate callback);
+        void subsrcibe_start_new_wave(player_state_delegate callback);
         
         bool is_placing() const { return _is_placing; }
 
@@ -23,6 +25,7 @@ namespace ppp
         std::vector<player_state_delegate> _start_placement_callbacks;
         std::vector<player_state_delegate> _stop_placement_callbacks;
         std::vector<player_state_delegate> _try_spawn_tower_callbacks;
+        std::vector<player_state_delegate> _new_wave_callbacks;
 
         bool _is_placing = false;
     };
